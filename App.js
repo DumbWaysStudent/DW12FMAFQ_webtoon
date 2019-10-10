5e1cc79a9c020dfe4f8fcf371769d7ff0144403a
noorqidam <noorqidam@gmail.com>
2019-10-10 09:56:18 +0700

    "add detail webtoon screen"

--- App.js
+++ App.js
@@ -1,8 +1,40 @@
+import React from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-
+import { Share, Alert } from 'react-native';
+import { Icon } from 'native-base'
 import Login from './src/screens/Login';
 import Foryou from './src/screens/Foryou';
+import DetailWebtoon from './src/screens/DetailWebtoon';
+
+const App = createSwitchNavigator({
+  App: {
+    screen: Login,
+  },
+  Auth: {
+    screen: AuthStack,
+  },
+});
+
+const onShare = async () => {
+  try {
+    const result = await Share.share({
+      message: 'Apps Webtoon',
+    });
+
+    if (result.action === Share.sharedAction) {
+      if (result.activityType) {
+        Alert.alert('1');
+      } else {
+        Alert.alert('2');
+      }
+    } else if (result.action === Share.dismissedAction) {
+      Alert.alert('3');
+    }
+  } catch (error) {
+    Alert.alert(error.message);
+  }
+};
 
 const AuthStack = createStackNavigator({
   ForYou: {
@@ -15,14 +47,22 @@ const AuthStack = createStackNavigator({
       },
     },
   },
-});
-
-const App = createSwitchNavigator({
-  App: {
-    screen: Login,
-  },
-  Auth: {
-    screen: AuthStack,
+  DetailWebtoon: {
+    screen: DetailWebtoon,
+    navigationOptions: {
+      headerTitle: 'The Secret Of Angel',
+      headerRight: <Icon light name="share" onPress={onShare} />,
+      headerStyle: {
+        backgroundColor: '#ffffff',
+      },
+      headerRightContainerStyle: {
+        marginEnd: 20,
+      },
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+      headerTintColor: 'black',
+    },
   },
 });
 
