9ffe6a8d7e8357812cab04dec464ceb2bf2565f4
Noor Qidam <45481403+noorqidam@users.noreply.github.com>
2019-10-19 20:03:19 +0700

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken')
 const models = require('../models')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const User = models.users
 
 exports.login = (req, res) => {
   const email = req.body.email
   const password = req.body.password
-
   User.findOne({ where: { email, password } }).then(user => {
-
     if (user) {
       const token = 'Bearer ' + jwt.sign({ userId: user.id }, 'my-secret-key')
       res.send({
@@ -24,19 +24,29 @@ exports.login = (req, res) => {
 }
 
 exports.register = (req, res) => {
-  User.findOne({ where: { email, password } }).then(user => {
-
+  const email = req.body.email
+  User.findOne({ where: { email } }).then(user => {
     if (user) {
-      const token = 'Bearer ' + jwt.sign({ userId: user.id }, 'my-secret-key')
-      res.send({
-        email,
-        token
-      })
-    } else {
       res.send({
         error: true,
-        message: "Email yang anda masukkan salah!"
+        message: 'Email Sudah Terdaftar',
       })
+    } else {
+      bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(req.body.password, salt, function (err, hash) {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+          }).then(user => {
+            const token = 'Bearer ' + jwt.sign({ userId: user.id }, 'my-secret-key')
+            res.send({
+              data: user,
+              token,
+            })
+          })
+        });
+      });
     }
   })
 }
